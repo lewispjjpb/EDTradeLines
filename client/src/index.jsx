faed729657543b4e101598c4b10c15b3736ab47e
Patrick Lewis <lewispjjpb@gmail.com>
2021-12-19 19:29:23 -0500

    Change stationList to array

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,9 @@
 import React, {Suspense} from 'react';
 import axios from 'axios';
+import { format } from 'timeago.js';
 import BuyGraph from './buyGraph.jsx';
 import SellGraph from './sellGraph.jsx';
-import { format } from 'timeago.js';
+
 
 class Main extends React.Component {
   constructor(props) {
@@ -10,21 +11,26 @@ class Main extends React.Component {
     this.state = {
       currentStation: {},
       stationList: [],
-      queueStation: ''
+      activeSuggestion: 0,
+      filtered: [],
+      showSuggestions: false,
+      queueStation: '',
     }
     this.getStation = this.getStation.bind(this);
     this.populateStationList = this.populateStationList.bind(this);
     this.setStation = this.setStation.bind(this);
-    this.updateStation = this.updateStation.bind(this);
+    this.getNewStation = this.getNewStation.bind(this);
   }
+
+  statList = [];
 
   componentDidMount() {
     this.getStation('Abernathy City');
     this.populateStationList();
   }
 
-  getStation(stationId) {
-    axios.get(`/market/${stationId}`)
+  getStation(stationName) {
+    axios.get(`/market/${stationName}`)
       .then(response => this.setState({'currentStation': response.data[0]}))
       .then(console.log('station loaded'))
       .catch('error in getStation?')
@@ -35,23 +41,47 @@ class Main extends React.Component {
       .then (response => {
         console.log('headers: ', response.headers)
         this.setState({stationList: response.data})
+        this.statList = response.data
+        console.log('set statList')
       })
   }
 
   setStation(e) {
     e.preventDefault();
-    this.setState({queueStation: e.target.value})
+    const userInput = e.target.value;
+    const filtered = this.statList.filter(
+      suggestion => suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    )
+    this.setState({
+      activeSuggestion: 0,
+      filtered,
+      showSuggestions: true,
+      queueStation: e.target.value
+    })
   }
 
-  updateStation(e) {
+  selectStationFromList(e) {
+    this.setState({
+      activeSuggestion: 0,
+      filtered: [],
+      showSuggestions: false,
+      queueStation: e.currentTarget.innerText
+    });
+  }
+
+
+
+
+
+
+
+  getNewStation(e) {
     e.preventDefault();
-    let name = [this.state.queueStation]
+    let name = this.state.queueStation
     this.getStation(name)
   }
 
   render() {
-    const simpleList = this.state.stationList
-    console.log('numstations: ', simpleList.length)
     const filterStations = function(array, query) {
       return array.filter(function(station) {
         return station.toLowerCase().indexOf(query.toLowerCase()) !== -1
@@ -62,40 +92,13 @@ class Main extends React.Component {
         <div className="header">
           <div>Current station: <b>{this.state.currentStation.stationName}   </b>
               ||| Market data age: {format(this.state.currentStation.date)} </div>
-          <form onSubmit={this.updateStation}>
-            <label>
-              Select station:
-              </label>
-              <input
-              type="text"
-              name="search"
-              id="selection"
-              value={this.state.queueStation}
-              placeholder="Type station name..."
-              onChange={this.setStation}
+              <br />
+              <select type="text" value={this.state.queueStation} onChange={this.setStation}>
+                {this.state.stationList.map(item => <option value={item}>{item}</option>)}
+              </select>
 
-              />
-              {/* {filterStations(simpleList, this.state.queueStation)} */}
-              {/* <script id="selectscript">{
-                function chooseStation() {
-                  console.log('y7ou are here')
-                  var station, filter, stationList, txtVal, a;
-                  station = this.state.queueStation;
-                  filter = station.toLowerCase();
-                  section = document.getElementById('selection')
-                  stationList = this.state.stationList;
-                  for (var i = 0; i < stationList.length; i++) {
-                    let a = stationList[i];
-                    txtVal = this.state.queueStation;
-                    if (txtVal.toLowerCase().indexOf(filter) > -1) {
-                      console.log(stationList[i]);
-                    }
-                  }
-                }
-              }words</script> */}
-
-            <input type="submit" value="Get station data" className="get"/>
-          </form>
+            <input type="submit" value="Get station data" className="get"
+            onClick={this.getNewStation}/>
         </div>
 
         <div className="main">
