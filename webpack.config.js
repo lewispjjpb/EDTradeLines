174f849ffb18f3ef14d8a394570cd6e64abe1a93
Patrick Lewis <lewispjjpb@gmail.com>
2022-02-15 14:24:54 -0500

    Remove old bundle

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,6 @@ const config = {
         test: /\.(js|jsx|ts|tsx)$/,
         use: {
           loader: 'babel-loader',
-          // options: {
-          //   presets: ['@babel/preset-react', '@babel/preset-env'],
-          // },
         },
         exclude: /node_modules/
       }
